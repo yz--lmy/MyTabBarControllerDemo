3ccbc4c17da39647c308f502cd55851874118827
lmy <lmy@lmydeMacBook-Pro.local>
2014-09-03 00:14:10 +0800

    Initial Commit

---
+++ MyTabBarControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTabBarControllerDemo
+//
+//  Created by lmy on 14-9-3.
+//  Copyright (c) 2014年 lmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
